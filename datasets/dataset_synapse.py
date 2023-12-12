bd53ff429ce10c4087e22f3536579e71c1b72b75
Tim7179 <77618801+Tim7179@users.noreply.github.com>
2023-12-12 12:59:10 +0800

    Update dataset_synapse.py

--- datasets/dataset_synapse.py
+++ datasets/dataset_synapse.py
@@ -1,11 +1,13 @@
 import os
 import random
-import h5py
+from PIL import Image
 import numpy as np
 import torch
 from scipy import ndimage
 from scipy.ndimage.interpolation import zoom
 from torch.utils.data import Dataset
+from torchvision.utils import save_image
+import h5py
 
 
 def random_rot_flip(image, label):
@@ -28,23 +30,29 @@ def random_rotate(image, label):
 class RandomGenerator(object):
     def __init__(self, output_size):
         self.output_size = output_size
+        self.train_imgs_path = "../train_save_imgs"
 
     def __call__(self, sample):
-        image, label = sample['image'], sample['label']
+        image, label= sample['image'], sample['label']
 
         if random.random() > 0.5:
             image, label = random_rot_flip(image, label)
         elif random.random() > 0.5:
             image, label = random_rotate(image, label)
-        x, y = image.shape
+        
+        x, y, _ = image.shape
         if x != self.output_size[0] or y != self.output_size[1]:
             image = zoom(image, (self.output_size[0] / x, self.output_size[1] / y), order=3)  # why not 3?
             label = zoom(label, (self.output_size[0] / x, self.output_size[1] / y), order=0)
+        #image = image.transpose(2,0,1)
+        #print(image.shape)
         image = torch.from_numpy(image.astype(np.float32)).unsqueeze(0)
         #image = torch.from_numpy(image.astype(np.float32))
         #image = image.permute(2, 0, 1)
         label = torch.from_numpy(label.astype(np.float32))
         sample = {'image': image, 'label': label.long()}
+        
+
         return sample
 
 
@@ -69,6 +77,30 @@ class Synapse_dataset(Dataset):
             filepath = self.data_dir + "/{}.npy.h5".format(vol_name)
             data = h5py.File(filepath)
             image, label = data['image'][:], data['label'][:]
+
+        sample = {'image': image, 'label': label}
+        if self.transform:
+            sample = self.transform(sample)
+        sample['case_name'] = self.sample_list[idx].strip('\n')
+        return sample
+
+
+
+class Breast_dataset(Dataset):
+    def __init__(self, base_dir, list_dir, split, transform=None):
+        self.transform = transform  # using transform in torch!
+        self.split = split
+        self.sample_list = open(os.path.join(list_dir, self.split+'.txt')).readlines()
+        self.data_dir = base_dir
+
+    def __len__(self):
+        return len(self.sample_list)
+
+    def __getitem__(self, idx):
+        slice_name = self.sample_list[idx].strip('\n')
+        data_path = os.path.join(self.data_dir, slice_name+'.npz')
+        data = np.load(data_path)
+        image, label = data['image'], data['label']
 
         sample = {'image': image, 'label': label}
         if self.transform:
